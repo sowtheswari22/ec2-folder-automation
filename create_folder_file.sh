7fb0b8aa54931d1123cb7998757d107a20b936af
sowtheswari22 <sowtheswari22@gmail.com>
2025-12-12 18:12:15 +0530

    Update create_folder_file.sh

--- create_folder_file.sh
+++ create_folder_file.sh
@@ -5,7 +5,7 @@ FOLDER_PATH="/opt/myfolder"
 FILE_PATH="$FOLDER_PATH/hello.txt"
 
 # Create folder if it doesn't exist
-mkdir -p $FOLDER_PATH
+sudo mkdir -p $FOLDER_PATH
 
 # Write content to file
-echo "Hello World" > $FILE_PATH
+echo "Hello World" | sudo tee $FILE_PATH
