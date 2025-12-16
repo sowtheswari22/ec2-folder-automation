def170b71577d066f8aa370e75a1d1a993f6b535
sowtheswari22 <sowtheswari22@gmail.com>
2025-12-16 13:48:50 +0530

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -2,5 +2,5 @@
 
 OUTPUT_FILE="/opt/hello_world.txt"
 
-echo "HELLO WORLD FRom JENKINS AUTOMATION" > $OUTPUT_FILE
+echo "HELLO WORLD FRom JENKINS and target AUTOMATION" > $OUTPUT_FILE
 date >> $OUTPUT_FILE
